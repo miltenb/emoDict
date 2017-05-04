ff683e366d6555621fd56d4707200f715ff9f218
Joost Miltenburg <miltenb@joosts-mbp.lan>
2017-05-04 15:43:51 +0200

    Initial Commit

---
+++ emoDict/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  emoDict
+//
+//  Created by Joost Miltenburg on 04/05/2017.
+//  Copyright © 2017 JMSC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
